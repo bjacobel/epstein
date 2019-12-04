f759532bbd627f2a49744e284d8afcfe0a9cf498
Brian Jacobel <bjacobel@gmail.com>
2019-12-04 17:19:46 -0500

    Flight search query good to go

--- backend/infra/appsync/resolvers.tf
+++ backend/infra/appsync/resolvers.tf
@@ -1,18 +1,24 @@
+locals {
+  resolver_dir = "${path.module}/../../resolvers"
+}
+
 resource "aws_appsync_resolver" "query_resolver" {
-  for_each    = fileset(path.module, "../../resolvers/*.sql")
-  api_id      = aws_appsync_graphql_api.appsync.id
-  type        = regex("(\\w+)\\.(\\w+)\\.sql", basename(each.value))[0]
-  field       = regex("(\\w+)\\.(\\w+)\\.sql", basename(each.value))[1]
+  for_each = fileset(local.resolver_dir, "**/request.sql")
+
+  type  = regex("(\\w+)\\.(\\w+)\\/", each.value)[0]
+  field = regex("(\\w+)\\.(\\w+)\\/", each.value)[1]
+
   data_source = aws_appsync_datasource.datasource.name
+  api_id      = aws_appsync_graphql_api.appsync.id
 
   request_template = <<EOF
 {
     "version": "2018-05-29",
     "statements":[
-        "${replace(file("${path.module}/${each.value}"), "\n", "\\n")}"
+        "${replace(file("${local.resolver_dir}/${each.value}"), "\n", "\\n")}"
     ]
 }
 EOF
 
-  response_template = file("${path.module}/../../resolvers/mappings/${basename(each.value)}.vtl")
+  response_template = file(replace("${local.resolver_dir}/${each.value}", basename(each.value), "response.vtl"))
 }
