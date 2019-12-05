438aaec56b1d39b23ddfac19c4e1ea3126c9ea2c
Brian Jacobel <bjacobel@gmail.com>
2019-12-04 22:52:25 -0500

    Much cleaner passengers resolver

--- backend/resolvers/Flight.destination/request.sql
+++
@@ -1,1 +0,0 @@
-select * from airports where iata_code = '$ctx.source.destination' or gps_code = '$ctx.source.destination'
--- backend/resolvers/Query.searchFlights/request.sql
+++
@@ -1,7 +0,0 @@
-select *
-from manifest
-left join meta
-using(id)
-where to_tsvector(remarks)
-@@ '$ctx.args.query'
-order by date asc
