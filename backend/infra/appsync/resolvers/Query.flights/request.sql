83849a110085522a228573579ae25bbb8c5ab96b
Brian Jacobel <bjacobel@gmail.com>
2019-12-07 16:57:09 -0500

    Pagination works except you can't not pass limit

--- backend/infra/appsync/resolvers/Query.flights/request.sql
+++ backend/infra/appsync/resolvers/Query.flights/request.sql
@@ -3,5 +3,5 @@ left join meta using(id)
 where meta.logistical = $ctx.args.includeLogistical
 or meta.logistical = false
 order by date asc
-limit $util.defaultIfNull($ctx.args.limit, 100)
+limit $util.defaultIfNull($ctx.args.limit, 100) + 1
 offset $util.defaultIfNull($ctx.args.offset, 0)
