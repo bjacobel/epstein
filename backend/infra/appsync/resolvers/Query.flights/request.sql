9afd324b0f7b71e7241241ba0c26027040b0d976
Brian Jacobel <bjacobel@gmail.com>
2019-12-07 13:47:52 -0500

    More work on pagination, list-flights query

---
+++ backend/infra/appsync/resolvers/Query.flights/request.sql
@@ -0,0 +1,7 @@
+select * from manifest
+left join meta using(id)
+where meta.logistical = $ctx.args.includeLogistical
+or meta.logistical = false
+order by date asc
+limit $util.defaultIfNull($ctx.args.limit, 100)
+offset $util.defaultIfNull($ctx.args.offset, 0)
