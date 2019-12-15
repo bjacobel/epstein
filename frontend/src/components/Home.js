68f4a5be91f80ec18a14fd75e779045f898e1710
Brian Jacobel <bjacobel@gmail.com>
2019-12-15 12:43:28 -0500

    Rename 'Main' to 'Home'

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 // import styles from '../stylesheets/main.css';
 
-export default class Main extends Component {
+export default class Home extends Component {
   render() {
     return <></>;
   }
