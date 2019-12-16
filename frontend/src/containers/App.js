61a3f05891f6d9ef177745255bd427fec107a32e
Brian Jacobel <bjacobel@gmail.com>
2019-12-15 23:14:46 -0500

    reorganize

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Routes from './Routes';
-import Header from './Header';
+import Header from '../components/Header';
 import { appWrap } from '../stylesheets/app.css';
 
 export default () => (
