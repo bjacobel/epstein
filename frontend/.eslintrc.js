7cfbdffdf841f61fa503cda505c983476ccfd90f
Brian Jacobel <bjacobel@gmail.com>
2019-12-18 18:15:05 -0500

    passenger page layout, sentry setup

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     'no-else-return': 0,
     'no-console': 0,
     'import/prefer-default-export': 1,
-    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/no-extraneous-dependencies': [
+      2,
+      { devDependencies: true, packageDir: __dirname },
+    ],
     'import/order': [
       2,
       {
