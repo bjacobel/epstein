5e19e5730c4b2adbcf5c6d6ac3a0ccfb2aa4c388
Brian Jacobel <bjacobel@gmail.com>
2019-12-23 18:23:10 -0600

    passengers listings link if links are available

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Map, TileLayer, Polyline, LayerGroup } from 'react-leaflet';
-import { latLngBounds, latLng } from 'leaflet';
+import { Map, TileLayer, LayerGroup, Polyline } from 'react-leaflet';
+import { latLngBounds, LatLng } from 'leaflet';
+import { GreatCircle } from 'arc';
 import 'leaflet/dist/leaflet.css';
 
 import { MAP_TILES, MAP_ATTRIBUTION } from '../../constants';
 import { mapContainer } from './style.css';
 
+const latLngToXY = ([lat, lng]) => ({
+  x: lng,
+  y: lat,
+});
+
 export default ({ source, dest }) => {
-  const bounds = latLngBounds(latLng(...source), latLng(...dest));
+  const leafletSource = new LatLng(...source);
+  const leafletDest = new LatLng(...dest);
+  const gc = new GreatCircle(latLngToXY(source), latLngToXY(dest));
+  const curve = gc.Arc(100);
+  const points = curve.geometries[0].coords.map(([y, x]) => new LatLng(x, y));
+  const bounds = latLngBounds(leafletSource, leafletDest);
+
   return (
     <Map
       className={mapContainer}
       bounds={bounds}
-      boundsOptions={{ padding: [50, 50] }}
+      boundsOptions={{ padding: [10, 10] }}
       dragging={false}
       zoomControl={false}
       scrollWheelZoom={false}
@@ -22,7 +34,7 @@ export default ({ source, dest }) => {
     >
       <TileLayer attribution={MAP_ATTRIBUTION} url={MAP_TILES} />
       <LayerGroup>
-        <Polyline color="black" interactive={false} positions={[source, dest]} />
+        <Polyline color="black" interactive={false} positions={points} />
       </LayerGroup>
     </Map>
   );
