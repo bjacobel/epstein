1c289c580e4266c34af6439e3c638b6e0fad8aef
Brian Jacobel <bjacobel@gmail.com>
2019-12-23 22:03:25 -0600

    write a test for map

---
+++ frontend/src/components/Map/__tests__/Map-test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Curve from 'react-leaflet-curve/src/Curve';
+
+import Map from '../Map';
+
+jest.mock('leaflet');
+jest.mock('react-leaflet-curve/src/Curve', () => jest.fn(() => null));
+
+describe('map component', () => {
+  describe('great circle computation', () => {
+    it('draws a curve with the right commands', () => {
+      const wrapper = mount(<Map source={[0, 0]} dest={[0, 10]} />);
+
+      console.log(wrapper.debug());
+
+      expect(wrapper.find(Curve).prop('positions')).toMatchInlineSnapshot(`
+        Array [
+          "M",
+          Array [
+            0,
+            0,
+          ],
+          "Q",
+          Array [
+            0,
+            4.999999999999999,
+          ],
+          Array [
+            0,
+            10,
+          ],
+        ]
+      `);
+    });
+  });
+});
