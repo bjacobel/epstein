88fa53a1f09583c897a32b883f46a28ccf55e1a8
Brian Jacobel <bjacobel@gmail.com>
2020-01-06 22:34:52 -0500

    Add link to second PDF on about page

--- backend/infra/files/pdfs.tf
+++ backend/infra/files/pdfs.tf
@@ -4,3 +4,10 @@ resource "aws_s3_bucket_object" "manifest09pdf" {
   source       = "../../data/manifests/Epstein-v-Edwards.pdf"
   content_type = "application/pdf"
 }
+
+resource "aws_s3_bucket_object" "manifest09pdf" {
+  bucket       = aws_s3_bucket.files.id
+  key          = "/manifest15.pdf"
+  source       = "../../data/manifests/Giuffre-v-Maxwell.pdf"
+  content_type = "application/pdf"
+}
