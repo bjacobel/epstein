20cecb96489e4ff28f7f246a3b5aed3a1323cc74
Brian Jacobel <bjacobel@gmail.com>
2020-05-13 15:02:40 -0400

    SearchResultsFlightConnection has a total count of all flights

--- backend/infra/appsync/resolvers/Query.searchRemarksForFlights/request.sql
+++ backend/infra/appsync/resolvers/Query.searchRemarksForFlights/request.sql
@@ -7,3 +7,10 @@ where to_tsvector(remarks)
 order by date asc
 limit $util.defaultIfNull($ctx.args.limit, 100) + 1
 offset $util.defaultIfNull($ctx.args.offset, 0)
+
+-- statementbreak
+
+select count(*)
+from manifest
+where to_tsvector(remarks)
+@@ '$ctx.args.query'
