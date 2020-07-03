05d7f0eb2c021988319b53412ec854f42e06647b
Brian Jacobel <bjacobel@gmail.com>
2020-07-02 20:12:02 -0400

    Form validation; prevent this from happening two ways

--- frontend/src/components/SearchBox/SearchBox.js
+++ frontend/src/components/SearchBox/SearchBox.js
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
 
-import { form, input, button } from './style.css';
+import { form, input, button, errorText } from './style.css';
 
 export default ({ initialValue, onClick }) => {
   const [searchQuery, updateSearchQuery] = useState(initialValue);
+  const [validationError, setValidationError] = useState();
 
   const search = e => {
     e.preventDefault();
+    if (!searchQuery || searchQuery.length < 1) {
+      setValidationError('Search query is required.');
+      return;
+    }
     onClick(searchQuery);
   };
 
   return (
-    <form className={form}>
-      <input
-        className={input}
-        placeholder="passenger name"
-        type="search"
-        value={searchQuery || ''}
-        onChange={e => updateSearchQuery(e.target.value)}
-      />
-      <button className={button} type="submit" onClick={search}>
-        search
-      </button>
-    </form>
+    <>
+      <form className={form}>
+        <input
+          className={input}
+          placeholder="passenger name"
+          type="search"
+          value={searchQuery || ''}
+          onChange={e => updateSearchQuery(e.target.value)}
+        />
+        <button className={button} type="submit" onClick={search}>
+          search
+        </button>
+      </form>
+      <div className={errorText}>
+        <p>{validationError}</p>
+      </div>
+    </>
   );
 };
